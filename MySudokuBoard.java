dca1c7ed1a1163fe29a121ad2415a7c9be076184
eceesay1 <abeceesay81@gmail.com>
2025-07-27 23:01:51 +0500

    Update MySudokuBoard.java

--- MySudokuBoard.java
+++ MySudokuBoard.java
@@ -4,23 +4,19 @@
 // HW #2 Core Topics: 2D arrays, Sets, Maps, Sudoku validation
 //
 // In this program a sudoku board validator is provided. 
-// It ensures that a 9x9 sudoku board meets the standard game rules: 
-// no repeating digits along rows, columns or mini grids 3x3. 
-// It also checks whether the board is solved completely (has each of the digits in 1-9 each exhibiting nine times).
-
-
-/*
-   This is started code for Crystal's Sudoku #2.
-   The code is not pretty, but it works.
-*/
 
 import java.util.*;
 import java.io.*;
 
+
+// It ensures that a 9x9 sudoku board meets the standard game rules: 
+// no duplicate digits along rows, columns or mini grids 3x3. 
+// It also checks whether the board is solved completely (has each of the digits in 1-9 each exhibiting nine times).
 public class MySudokuBoard {
     public final int SIZE = 9;
     protected char[][] myBoard;
-
+   // Pre: theFile is a valid filename to the path of the 9x9 sudoku file whose line contains 9 characters
+   // Post: Initializes myBoard from the contents of the file
     public MySudokuBoard(String theFile) {
         myBoard = new char[SIZE][SIZE];
         try {
@@ -36,10 +32,14 @@ public class MySudokuBoard {
             e.printStackTrace();
         }
     }
+    
+   // Pre: myBoard is a 9 x9 array of character initialized
+   // Post: Returns true when there are no invalid characters on the board and that the board follows the rules of the game of Sudoku
     public boolean isValid() {
         return isValidChars() && isValidRows() && isValidCols() && isValidGrids();
     }
-
+   // Pre: myBoard is full of characters
+   // Post: Returns true when each of the characters consists of 1-9 or '.'
     private boolean isValidChars() {
         Set < Character > checkChars = new HashSet < > (Arrays.asList('1', '2', '3', '4', '5', '6', '7', '8', '9', '.'));
         for (int row = 0; row < SIZE; row++) {
@@ -52,6 +52,8 @@ public class MySudokuBoard {
         return true;
     }
 
+   // Pre: myBoard is valid 9x9 array of characters
+   // Post: Returns true if no rows contains duplicate digits 1-through 9
     private boolean isValidRows() {
         for (int row = 0; row < SIZE; row++) {
             Set < Character > listSet = new HashSet < > ();
@@ -66,6 +68,8 @@ public class MySudokuBoard {
         return true;
     }
 
+   // Pre: myBoard is valid 9x9 array of characters
+   // Post: Returns true if no columns contains duplicate digits 1-through 9
     private boolean isValidCols() {
         for (int col = 0; col < SIZE; col++) {
             Set < Character > listSet = new HashSet < > ();
@@ -79,6 +83,8 @@ public class MySudokuBoard {
         }
         return true;
     }
+   // Pre: myBoard is valid 9x9 array of characters
+   // Post: Returns true if all 3x3 subgrids contains duplicate digits 1-through 9
 
     private boolean isValidGrids() {
         for (int sRow = 0; sRow < SIZE; sRow += 3) {
@@ -97,6 +103,8 @@ public class MySudokuBoard {
         }
         return true;
     }
+   // Pre: myBoard initializes and is valid
+   // Post: It returns true on seeing that board is cleared out (all digits 1-9 appear in the board exactly 9 times)
     public boolean isSolved() {
         if (!isValid()) return false;
 
@@ -115,7 +123,8 @@ public class MySudokuBoard {
 
         return true;
     }
-
+   // Pre: myBoard initializes and is valid
+   // Post: String representation of the board returns
     public String toString() {
         String result = "My Board:\n\n";
         for (int row = 0; row < SIZE; row++) {
